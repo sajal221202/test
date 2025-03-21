067d4d456ef6bc4f034001f5d4281de73aa27429
Sajal Gupta <133880348+sajal221202@users.noreply.github.com>
2025-03-21 12:29:55 +0530

    Delete src directory

--- src/pages/api/tools/index.ts
+++
@@ -1,9 +0,0 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
-
-import indexTools from "../../../utils/indexTools";
-
-export default async (req: VercelRequest, res: VercelResponse) => {
-  const tools = await indexTools();
-
-  return res.status(200).json(tools);
-};
